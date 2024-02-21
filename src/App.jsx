593ed6bf99333051dad3f6da2b3e0c9bcd3e551a
ibaddkhan <ibadurrehman718@gmail.com>
2024-02-21 16:59:14 +0500

    initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [arr, setArr] = useState(null)
 
+  useEffect(() => {
+    axios.get("http://localhost:3000/").then((res) => {
+      console.log(res.data);
+      setArr(res.data)
+    })
+  }, [])
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+        {
+          arr && arr.map((item, i) => (
+            <ul key={i}>
+              <li>{item.name}</li>
+              <li>{item.age}</li>
+              <li>{item.city}</li>
+              <button>Delete</button>
+            </ul>
+          ))
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
